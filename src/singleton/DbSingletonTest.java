8ac3bc782cc5f76de427e9325d9d6a859f4637d7
Thayna Caixeta <thayna.caixeta@gmail.com>
2021-08-05 15:41:28 +0100

    Create a simple singleton

---
+++ src/singleton/DbSingletonTest.java
@@ -0,0 +1,20 @@
+package singleton;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+class DbSingletonTest {
+
+	@Test
+	void shouldHaveJustOneInstance() {
+		DbSingleton instance = DbSingleton.getInstance();		
+		DbSingleton anotherInstance = DbSingleton.getInstance();
+		
+		assertEquals(instance, anotherInstance);
+	}
+
+}
