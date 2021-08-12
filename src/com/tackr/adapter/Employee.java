8cb6b1b7960e693766b1839c466473474db5357c
Thayna Caixeta <thayna.caixeta@gmail.com>
2021-08-12 12:25:52 +0100

    Implemment an example for the Adapter design pattern

---
+++ src/com/tackr/adapter/Employee.java
@@ -0,0 +1,9 @@
+package com.tackr.adapter;
+
+public interface Employee {
+	
+	public String getId();
+	public String getFirstName();
+	public String getLastName();
+	public String getEmail();
+}
---
+++ src/com/tackr/adapter/EmployeeAdapterCSV.java
@@ -0,0 +1,35 @@
+package com.tackr.adapter;
+
+public class EmployeeAdapterCSV implements Employee {
+
+	private EmployeeCSV instance;
+
+	public EmployeeAdapterCSV(EmployeeCSV instance) {
+		this.instance = instance;
+	}
+
+	@Override
+	public String getId() {
+		return Integer.toString(instance.getId());
+	}
+
+	@Override
+	public String getFirstName() {
+		return instance.getFirstname();
+	}
+
+	@Override
+	public String getLastName() {
+		return instance.getLastname();
+	}
+
+	@Override
+	public String getEmail() {
+		return instance.getEmailAddress();
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeAdapterCSV" + instance.toString();
+	}
+}
